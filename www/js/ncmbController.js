7c143facfbdfc25d5843b39bc00499431de3cf72
k18rs050 <k18rs050@st.kyusan-u.ac.jp>
2019-12-02 14:10:52 +0900

    スコアを保存する

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -1,18 +1,40 @@
 // ncmbController.js
 
 var ncmbController = {
-  APLICATION_KEY:appKey,
+  APPLICATION_KEY:appKey,
   CLIENT_KEY:clientKey,
 
   ncmb:null,
   currentUser:null,
   screenSize:null,
 
+
+  // スコア送信
+  sendScore: function(score) {
+      var self = this;
+
+      // [1]Score（クラス）を生成
+      var Score = self.ncmb.DataStore("ScoreClass");
+
+      // [2]インスタンス生成、スコア数値をフィールド名"score"にセット
+      var scoreData = new Score({score: score});
+
+      // [3]送信処理
+      scoreData.save()
+         .then(function (saved) {
+         alert("スコア送信完了！");
+          })
+          .catch(function(err){
+          console.log(err);
+          });
+  },
+  //初期化
   init: function(screenSize) {
         var self = this;
         self.ncmb = new NCMB(self.APPLICATION_KEY, self.CLIENT_KEY);    // mobile backendの初期化
         self.screenSize = screenSize;
-    }
+        }
+
 }
 
 
